fcd2363cf85391bf80423b9af4f5b0a12e898f4f
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-09-01 14:42:10 +0600

    protected all the cart route

--- routes/cart/index.js
+++ routes/cart/index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router
     .get('/all', validateToken, Cart.getAllCartItems)
-    .get('/details/:userId', Cart.getCartByUserId)
+    .get('/details/:userId', validateToken, Cart.getCartByUserId)
     .post('/create', validateToken, Cart.addToCart);
 
 exports.router = router;
