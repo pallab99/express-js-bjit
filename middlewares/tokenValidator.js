77e7dcef3d3f394524078e4cef6e857248970f54
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-09-05 15:37:33 +0600

    done for today

--- middlewares/tokenValidator.js
+++ middlewares/tokenValidator.js
@@ -1,44 +1,7 @@
 const { failure } = require('../common/response');
 const jwt = require('jsonwebtoken');
 const dotEnv = require('dotenv');
-const userModel = require('../model/user');
 dotEnv.config();
-
-// const AUTH_FAILURE_MSG = 'Authorization failed.Please Sign in.';
-
-// const validateToken = async (req, res, next) => {
-//     const { authorization } = req.headers;
-//     const token = authorization?.split(' ')[1];
-//     const charset = process.env.ACCESS_TOKEN_SECRET;
-//     if (token === undefined) {
-//         res.status(401).json(failure(AUTH_FAILURE_MSG));
-//     } else {
-//         try {
-//             const decoded = await verifyToken(token, charset);
-//             const { id } = decoded;
-//             const user = await userModel.findById(id);
-//             if (user && !user.isValidSession) {
-//                 res.status(401).json(failure(AUTH_FAILURE_MSG));
-//             } else {
-//                 next();
-//             }
-//         } catch (err) {
-//             res.status(401).json(failure(AUTH_FAILURE_MSG));
-//         }
-//     }
-// };
-
-// const verifyToken = (token, charset) => {
-//     return new Promise((resolve, reject) => {
-//         jwt.verify(token, charset, (err, decoded) => {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(decoded);
-//             }
-//         });
-//     });
-// };
 
 const tokenAuthorization = (req, res, next) => {
     try {
--- util/tokenGenerator.js
+++ util/tokenGenerator.js
@@ -6,7 +6,7 @@ const generateSecretToken = (body) => {
     console.log({ body });
     // delete body.exp;
     const charset = process.env.ACCESS_TOKEN_SECRET;
-    const token = jwt.sign(body, charset, { expiresIn: '1 h' });
+    const token = jwt.sign(body, charset, { expiresIn: '20 s' });
     return token;
 };
 
