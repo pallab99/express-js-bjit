4eb788813eb2ab79bf3bcc76fff1711350df2312
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-09-05 16:32:48 +0600

    done the today's task

--- util/tokenGenerator.js
+++ util/tokenGenerator.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 
 const generateSecretToken = (body) => {
     console.log({ body });
-    // delete body.exp;
     const charset = process.env.ACCESS_TOKEN_SECRET;
     const token = jwt.sign(body, charset, { expiresIn: '20 s' });
     return token;
