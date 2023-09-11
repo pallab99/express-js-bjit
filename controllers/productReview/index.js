da46e1c75bd210fd2c422595464efc2374c2e392
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-09-11 15:45:03 +0600

    added a featire to delete review

--- controllers/productReview/index.js
+++ controllers/productReview/index.js
@@ -39,38 +39,73 @@ class productReviewController {
                 return res.status(200).json(failure('No product found'));
             }
             if (productExist) {
-                productExist.reviews.push({
-                    user: user,
-                    message: message,
-                    rating: rating,
-                });
-                await productExist.save();
-                const sum = productExist.reviews.reduce(
-                    (accumulator, review) => accumulator + review.rating,
-                    0
+                const userRatingExist = productExist.reviews.findIndex(
+                    (ele) => {
+                        return String(ele.user) === user;
+                    }
                 );
+                if (userRatingExist != -1) {
+                    (productExist.reviews[userRatingExist].user = user),
+                        (productExist.reviews[userRatingExist].message =
+                            message),
+                        (productExist.reviews[userRatingExist].rating = rating),
+                        await productExist.save();
+                    const sum = productExist.reviews.reduce(
+                        (accumulator, review) => accumulator + review.rating,
+                        0
+                    );
+                    const avg = sum / productExist.reviews.length;
 
-                const avg = sum / productExist.reviews.length;
+                    productExist.averageRating = avg;
+                    await productExist.save();
 
-                productExist.averageRating = avg;
-                await productExist.save();
-
-                const data = {
-                    _id: productExist?._id,
-                    product: productExist?.product,
-                    averageRating: productExist?.averageRating.toFixed(2),
-                    review: productExist?.reviews[
-                        productExist.reviews.length - 1
-                    ],
-                };
-                if (productExist) {
+                    const data = {
+                        _id: productExist?._id,
+                        product: productExist?.product,
+                        averageRating: productExist?.averageRating.toFixed(2),
+                        review: productExist?.reviews[
+                            productExist.reviews.length - 1
+                        ],
+                    };
                     return res
                         .status(200)
                         .json(success('Successfully created the review', data));
                 } else {
-                    return res
-                        .status(200)
-                        .json(failure('Something went wrong'));
+                    productExist.reviews.push({
+                        user: user,
+                        message: message,
+                        rating: rating,
+                    });
+                    await productExist.save();
+                    const sum = productExist.reviews.reduce(
+                        (accumulator, review) => accumulator + review.rating,
+                        0
+                    );
+
+                    const avg = sum / productExist.reviews.length;
+
+                    productExist.averageRating = avg;
+                    await productExist.save();
+
+                    const data = {
+                        _id: productExist?._id,
+                        product: productExist?.product,
+                        averageRating: productExist?.averageRating.toFixed(2),
+                        review: productExist?.reviews[
+                            productExist.reviews.length - 1
+                        ],
+                    };
+                    if (data) {
+                        return res
+                            .status(200)
+                            .json(
+                                success('Successfully created the review', data)
+                            );
+                    } else {
+                        return res
+                            .status(200)
+                            .json(failure('Something went wrong'));
+                    }
                 }
             } else {
                 const result = await productReviewModel.create({
@@ -194,6 +229,49 @@ class productReviewController {
             res.status(200).json(
                 success('successfully get the data', userReviews)
             );
+        } catch (error) {
+            console.log(error);
+            res.status(500).json(failure('Internal Server Error'));
+        }
+    }
+
+    async deleteReview(req, res) {
+        try {
+            const { user } = req.body;
+            const { productId } = req.params;
+            if (!mongoose.Types.ObjectId.isValid(user)) {
+                return res
+                    .status(400)
+                    .json(failure('Invalid user id provided'));
+            }
+            if (!mongoose.Types.ObjectId.isValid(productId)) {
+                return res
+                    .status(400)
+                    .json(failure('Invalid product id provided'));
+            }
+            var objectId = new mongoose.Types.ObjectId(productId);
+            const result = await productReviewModel.findOne({
+                product: objectId,
+            });
+
+            if (result) {
+                const findUserReview = result.reviews.findIndex(
+                    (ele) => String(ele.user) === user
+                );
+
+                console.log({ findUserReview });
+                if (findUserReview === -1) {
+                    return res.status(500).json(failure('Wrong user id'));
+                }
+
+                result.reviews.splice(findUserReview, 1);
+                await result.save();
+                return res
+                    .status(500)
+                    .json(success('Successfully deleted review'));
+            } else {
+                res.status(500).json(failure('Something went wrong'));
+            }
         } catch (error) {
             console.log(error);
             res.status(500).json(failure('Internal Server Error'));
--- routes/productReview/index.js
+++ routes/productReview/index.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router
     .post('/create', validator.addProductReview, productReview.addRating)
     .get('/getReviewByProduct/:productId', productReview.getReviewByProduct)
-    .get('/getReviewByUser/:userId', productReview.getReviewByUser);
+    .get('/getReviewByUser/:userId', productReview.getReviewByUser)
+    .delete('/deleteReview/:productId', productReview.deleteReview);
 
 exports.router = router;
